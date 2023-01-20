74fafe9435af29f614826e953e425d14f3c72a36
spac-robert <spacrobert.florian@gmail.com>
2023-01-20 14:22:00 +0200

    Added pagination but needts to retriev all products

--- src/app/products/component/product/products.component.ts
+++ src/app/products/component/product/products.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../../service/product.service";
 import {ProductModel} from "../../../dto/product.model";
-import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-products',
@@ -13,7 +12,7 @@ export class ProductsComponent implements OnInit {
   product: ProductModel | undefined
   totalLength: any;
   page: number = 0
-  pageSize: number = 7
+  pageSize: number = 10
   sortBy: string = "name"
   sortDir: string = "asc"
   products$: ProductModel[] | undefined
@@ -21,11 +20,16 @@ export class ProductsComponent implements OnInit {
 
   constructor(private service: ProductService) {
   }
+
   //TODO de facut paginarea
+  //TODO sa setes sort-ul dupa anumite criterii si tipul de sortare asc sau desc si page number astea sa se ia
+  //din html. Exemplu e pe ChatGPT am si ss facut cu asta
 
   //TODO imi apar produsele, dar dupa ce dau refresh imi dispar
   ngOnInit(): void {
     this.products$ = this.service.getProducts(this.pageSize, this.page, this.sortBy, this.sortDir);
+    this.totalLength = this.products$.length
+
     // //sa iau toate produsele in products
     // this.service.getProducts().subscribe((result) => {
     //   this.products = result;
@@ -37,4 +41,7 @@ export class ProductsComponent implements OnInit {
 
   }
 
+  getProducts(sortBy: string, sortDir: string, $event: number, pageSize: number) {
+    this.products$ = this.service.getProducts(pageSize, this.page, sortBy, sortDir);
+  }
 }
