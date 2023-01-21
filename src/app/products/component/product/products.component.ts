8dfabc633eb5aadc21e917988e40d53786e32ff9
spac-robert <spacrobert.florian@gmail.com>
2023-01-21 03:43:51 +0200

    added sort by name and manufacturer

--- src/app/products/component/product/products.component.ts
+++ src/app/products/component/product/products.component.ts
@@ -12,7 +12,7 @@ export class ProductsComponent implements OnInit {
   product: ProductModel | undefined
   totalLength: any;
   page: number = 0
-  pageSize: number = 10
+  pageSize: number = 100
   sortBy: string = "name"
   sortDir: string = "asc"
   products$: ProductModel[] | undefined
@@ -21,27 +21,29 @@ export class ProductsComponent implements OnInit {
   constructor(private service: ProductService) {
   }
 
-  //TODO de facut paginarea
+  //TODO sa adaug stock la produse
   //TODO sa setes sort-ul dupa anumite criterii si tipul de sortare asc sau desc si page number astea sa se ia
   //din html. Exemplu e pe ChatGPT am si ss facut cu asta
 
   //TODO imi apar produsele, dar dupa ce dau refresh imi dispar
+  //TODO sa fac un sort-control-group ca pe emag cu sort si filters
+  selectedSort: string = "name";
+
   ngOnInit(): void {
     this.products$ = this.service.getProducts(this.pageSize, this.page, this.sortBy, this.sortDir);
     this.totalLength = this.products$.length
-
-    // //sa iau toate produsele in products
-    // this.service.getProducts().subscribe((result) => {
-    //   this.products = result;
-    //   this.totalLength = result.length;
-    //
-    // })
-    //this.product = this.service.getProductByCode("1")
-    //this.url = "/products/p/" + this.product?.id;
 
   }
 
   getProducts(sortBy: string, sortDir: string, $event: number, pageSize: number) {
     this.products$ = this.service.getProducts(pageSize, this.page, sortBy, sortDir);
+  }
+
+  sortProducts() {
+    if (this.selectedSort === 'name') {
+      this.products$?.sort((a, b) => a.name.localeCompare(b.name));
+    } else {
+      this.products$?.sort((a, b) => a.manufacturer.localeCompare(b.manufacturer));
+    }
   }
 }
